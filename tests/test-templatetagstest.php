0e188555d2280fa61e331ee541673440fc866a5b
Johnny Huynh <johnnyhuy@users.noreply.github.com>
2017-11-09 14:18:20 +1100

    Refactored tests.

--- tests/test-templatetagstest.php
+++ tests/test-templatetagstest.php
@@ -22,127 +22,164 @@ class TemplateTagsTest extends WP_UnitTestCase {
 	 * Test to get the elapsed time of
 	 *  a post in minutes.
 	 *
+	 * We'll be testing lower, middle and upper bounds.
+	 *
 	 * @return void
 	 */
 	function test_get_elapsed_time_string_minutes() {
-		$time = new DateTime( '-1 minute' );
-		$post = self::factory()->post->create_and_get( array(
-			'post_date'     => $time->format( 'Y-m-d H:i:s' ),
-			'post_modified' => $time->format( 'Y-m-d H:i:s' ),
-		) );
-		$this->assertEquals( '1 minute', get_elapsed_time_string( false, $post->ID ) );
-		$this->assertEquals( '1 minute', get_elapsed_time_string( true, $post->ID ) );
+		/**
+		 * Test data.
+		 *  where key is the time subtracted from now.
+		 *  and value is the expected result.
+		 */
+		$test_data = array(
+			'60 seconds' => '1 minute',
+			'1 minute'   => '1 minute',
+			'30 minutes' => '30 minutes',
+			'59 minutes' => '59 minutes',
+		);
 
-		$time = new DateTime( '-10 minutes' );
-		$post = self::factory()->post->create_and_get( array(
-			'post_date'     => $time->format( 'Y-m-d H:i:s' ),
-			'post_modified' => $time->format( 'Y-m-d H:i:s' ),
-		) );
-		$this->assertEquals( '10 minutes', get_elapsed_time_string( false, $post->ID ) );
-		$this->assertEquals( '10 minutes', get_elapsed_time_string( true, $post->ID ) );
+		foreach ( $test_data as $test_item => $expected_item ) {
+			$time = new DateTime( '-' . $test_item );
+			$post = self::factory()->post->create_and_get( array(
+				'post_date'     => $time->format( 'Y-m-d H:i:s' ),
+				'post_modified' => $time->format( 'Y-m-d H:i:s' ),
+			) );
+			$this->assertEquals( $expected_item, get_elapsed_time_string( false, $post->ID ) );
+			$this->assertEquals( $expected_item, get_elapsed_time_string( true, $post->ID ) );
+		}
 	}
 
 	/**
 	 * Test to get the elapsed time of
 	 *  a post in hours.
 	 *
+	 * We'll be testing lower, middle and upper bounds.
+	 *
 	 * @return void
 	 */
 	function test_get_elapsed_time_string_hours() {
-		$time = new DateTime( '-1 hour' );
-		$post = self::factory()->post->create_and_get( array(
-			'post_date'     => $time->format( 'Y-m-d H:i:s' ),
-			'post_modified' => $time->format( 'Y-m-d H:i:s' ),
-		) );
+		/**
+		 * Test data.
+		 *  where key is the time subtracted from now.
+		 *  and value is the expected result.
+		 */
+		$test_data = array(
+			'60 minutes' => '1 hour',
+			'1 hour'     => '1 hour',
+			'12 hours'   => '12 hours',
+			'23 hours'   => '23 hours',
+		);
 
-		$this->assertEquals( '1 hour', get_elapsed_time_string( false, $post->ID ) );
-		$this->assertEquals( '1 hour', get_elapsed_time_string( true, $post->ID ) );
-
-		$time = new DateTime( '-2 hours' );
-		$post = self::factory()->post->create_and_get( array(
-			'post_date'     => $time->format( 'Y-m-d H:i:s' ),
-			'post_modified' => $time->format( 'Y-m-d H:i:s' ),
-		) );
-
-		$this->assertEquals( '2 hours', get_elapsed_time_string( false, $post->ID ) );
-		$this->assertEquals( '2 hours', get_elapsed_time_string( true, $post->ID ) );
+		foreach ( $test_data as $test_item => $expected_item ) {
+			$time = new DateTime( '-' . $test_item );
+			$post = self::factory()->post->create_and_get( array(
+				'post_date'     => $time->format( 'Y-m-d H:i:s' ),
+				'post_modified' => $time->format( 'Y-m-d H:i:s' ),
+			) );
+			$this->assertEquals( $expected_item, get_elapsed_time_string( false, $post->ID ) );
+			$this->assertEquals( $expected_item, get_elapsed_time_string( true, $post->ID ) );
+		}
 	}
 
 	/**
 	 * Test to get the elapsed time of
 	 *  a post in days.
 	 *
+	 * We'll be testing lower, middle and upper bounds.
+	 *
 	 * @return void
 	 */
 	function test_get_elapsed_time_string_days() {
-		$time = new DateTime( '-1 day' );
-		$post = self::factory()->post->create_and_get( array(
-			'post_date'     => $time->format( 'Y-m-d H:i:s' ),
-			'post_modified' => $time->format( 'Y-m-d H:i:s' ),
-		) );
+		/**
+		 * Test data.
+		 *  where key is the time subtracted from now.
+		 *  and value is the expected result.
+		 */
+		$test_data = array(
+			'24 hours' => '1 day',
+			'1 day'    => '1 day',
+			'15 days'  => '15 days',
+			'29 days'  => '29 days',
+			'30 days'  => '30 days',
+		);
 
-		$this->assertEquals( '1 day', get_elapsed_time_string( false, $post->ID ) );
-		$this->assertEquals( '1 day', get_elapsed_time_string( true, $post->ID ) );
-
-		$time = new DateTime( '-2 days' );
-		$post = self::factory()->post->create_and_get( array(
-			'post_date'     => $time->format( 'Y-m-d H:i:s' ),
-			'post_modified' => $time->format( 'Y-m-d H:i:s' ),
-		) );
-
-		$this->assertEquals( '2 days', get_elapsed_time_string( false, $post->ID ) );
-		$this->assertEquals( '2 days', get_elapsed_time_string( true, $post->ID ) );
+		foreach ( $test_data as $test_item => $expected_item ) {
+			$time = new DateTime( '-' . $test_item );
+			$post = self::factory()->post->create_and_get( array(
+				'post_date'     => $time->format( 'Y-m-d H:i:s' ),
+				'post_modified' => $time->format( 'Y-m-d H:i:s' ),
+			) );
+			$this->assertEquals( $expected_item, get_elapsed_time_string( false, $post->ID ) );
+			$this->assertEquals( $expected_item, get_elapsed_time_string( true, $post->ID ) );
+		}
 	}
 
 	/**
 	 * Test to get the elapsed time of
 	 *  a post in months.
 	 *
+	 * We'll be testing lower, middle and upper bounds.
+	 *
 	 * @return void
 	 */
 	function test_get_elapsed_time_string_months() {
-		$time = new DateTime( '-1 month' );
-		$post = self::factory()->post->create_and_get( array(
-			'post_date'     => $time->format( 'Y-m-d H:i:s' ),
-			'post_modified' => $time->format( 'Y-m-d H:i:s' ),
-		) );
+		/**
+		 * Test data.
+		 *  where key is the time subtracted from now.
+		 *  and value is the expected result.
+		 */
+		$test_data = array(
+			'31 days'   => '1 month',
+			'32 days'   => '1 month',
+			'1 month'   => '1 month',
+			'6 months'  => '6 months',
+			'11 months' => '11 months',
+			'364 days'  => '11 months',
+		);
 
-		$this->assertEquals( '1 month', get_elapsed_time_string( false, $post->ID ) );
-		$this->assertEquals( '1 month', get_elapsed_time_string( true, $post->ID ) );
-
-		$time = new DateTime( '-2 months' );
-		$post = self::factory()->post->create_and_get( array(
-			'post_date'     => $time->format( 'Y-m-d H:i:s' ),
-			'post_modified' => $time->format( 'Y-m-d H:i:s' ),
-		) );
-
-		$this->assertEquals( '2 months', get_elapsed_time_string( false, $post->ID ) );
-		$this->assertEquals( '2 months', get_elapsed_time_string( true, $post->ID ) );
+		foreach ( $test_data as $test_item => $expected_item ) {
+			$time = new DateTime( '-' . $test_item );
+			$post = self::factory()->post->create_and_get( array(
+				'post_date'     => $time->format( 'Y-m-d H:i:s' ),
+				'post_modified' => $time->format( 'Y-m-d H:i:s' ),
+			) );
+			$this->assertEquals( $expected_item, get_elapsed_time_string( false, $post->ID ) );
+			$this->assertEquals( $expected_item, get_elapsed_time_string( true, $post->ID ) );
+		}
 	}
 
 	/**
 	 * Test to get the elapsed time of
 	 *  a post in years.
 	 *
+	 * We'll be testing lower, middle and upper bounds.
+	 *
 	 * @return void
 	 */
 	function test_get_elapsed_time_string_years() {
-		$time = new DateTime( '-1 year' );
-		$post = self::factory()->post->create_and_get( array(
-			'post_date'     => $time->format( 'Y-m-d H:i:s' ),
-			'post_modified' => $time->format( 'Y-m-d H:i:s' ),
-		) );
+		/**
+		 * Test data.
+		 *  where key is the time subtracted from now.
+		 *  and value is the expected result.
+		 */
+		$test_data = array(
+			'365 days'  => '1 year',
+			'366 days'  => '1 year',
+			'12 months' => '1 year',
+			'1 year'    => '1 year',
+			'6 years'   => '6 years',
+			'64 years'  => '64 years',
+		);
 
-		$this->assertEquals( '1 year', get_elapsed_time_string( false, $post->ID ) );
-		$this->assertEquals( '1 year', get_elapsed_time_string( true, $post->ID ) );
-
-		$time = new DateTime( '-2 years' );
-		$post = self::factory()->post->create_and_get( array(
-			'post_date'     => $time->format( 'Y-m-d H:i:s' ),
-			'post_modified' => $time->format( 'Y-m-d H:i:s' ),
-		) );
-
-		$this->assertEquals( '2 years', get_elapsed_time_string( false, $post->ID ) );
-		$this->assertEquals( '2 years', get_elapsed_time_string( true, $post->ID ) );
+		foreach ( $test_data as $test_item => $expected_item ) {
+			$time = new DateTime( '-' . $test_item );
+			$post = self::factory()->post->create_and_get( array(
+				'post_date'     => $time->format( 'Y-m-d H:i:s' ),
+				'post_modified' => $time->format( 'Y-m-d H:i:s' ),
+			) );
+			$this->assertEquals( $expected_item, get_elapsed_time_string( false, $post->ID ) );
+			$this->assertEquals( $expected_item, get_elapsed_time_string( true, $post->ID ) );
+		}
 	}
 }
