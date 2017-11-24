a325ebc79aed624c89124df4b3df400c6a1e9342
Johnny Huynh <johnnyhuy@users.noreply.github.com>
2017-11-25 02:13:33 +1100

    Hide intro for page other than home or front page.

--- index.php
+++ index.php
@@ -9,12 +9,18 @@
 
 ?>
 <?php get_header(); ?>
+
+<?php if ( is_front_page() || is_home() ) : ?>
+
 <div class="intro">
 	<section class="container intro-container">
 		<p class="intro-quote"><?php echo esc_html( get_option( 'jhuy_quote' ) ); ?></p>
 		<img class="intro-image" src="<?php echo esc_url( get_option( 'jhuy_quote_image' ) ); ?>" alt="jhuy_quote_image">
 	</section>
 </div>
+
+<?php endif; ?>
+
 <main id="main" class="container site-main" role="main">
 	<?php if ( is_single() ) : ?>
 		<div class="site-blog">
