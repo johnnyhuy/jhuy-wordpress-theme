9aed9f75234749c0b8a29e8eccc0c5fbf3a31734
Johnny Huynh <johnnyhuy@users.noreply.github.com>
2017-11-25 02:13:53 +1100

    Custom logo or text heading.

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -7,16 +7,21 @@
 ?>
 <div class="site-branding">
 
-	<?php the_custom_logo(); ?>
 
-	<?php if ( ! has_custom_logo() ) : ?>
+	<?php if ( has_custom_logo() ) : ?>
+
+		<?php the_custom_logo(); ?>
+
+	<?php else : ?>
+
 		<div class="site-branding-text">
 			<h2 class="site-title">
 				<a class="site-title-link"
 					href="<?php echo esc_url( home_url( '/' ) ); ?>"
 					rel="home"><?php bloginfo( 'name' ); ?></a>
-			</h2>
+			</h2><!-- .site-title -->
 		</div><!-- .site-branding-text -->
+
 	<?php endif; ?>
 
 </div>
