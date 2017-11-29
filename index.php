65207d973dcf5fc2c8e6b343d0829683ccdaf4b4
Johnny <johnnyhuy@users.noreply.github.com>
2017-11-30 00:49:49 +1100

    Sticky posts pinned on all blog pages.

--- index.php
+++ index.php
@@ -7,37 +7,69 @@
  * @since 1.0
  */
 
+// Only show pagination when page number is not 1.
+$paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+
+// Setup custom query to ignore sticky posts.
+$the_query = new WP_Query( array(
+	'post__not_in'   => get_option( 'sticky_posts' ),
+	'posts_per_page' => 5,
+	'paged'          => $paged,
+) );
+
+// Overwrite wp_query global with custom query.
+$wp_query = $the_query;
+
+
 ?>
 <?php get_header(); ?>
 
-	<main id="main" class="container container-sidebar site-main" role="main">
+	<di id="main" class="container container-sidebar site-main" role="main">
 
 		<?php
-		// Only show blog bar if is front page or home.
-		if ( is_front_page() || is_home() ) {
-			$blog_class = 'site-blog site-blog-bar';
-		} else {
-			$blog_class = 'site-blog';
-		}
-
-		// Only show pagination when page number is not 1.
-		$paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
-
+		/**
+		 * Only show top pagination
+		 * if page number is not 1 (front page).
+		 */
 		if ( 1 !== $paged ) {
 			jhuy_get_pagination();
 		}
 		?>
 
-		<div class="<?php echo $blog_class; ?>">
+		<div class="site-blog site-blog-sticky">
 
 		<?php
 
-		if ( have_posts() ) {
+		/**
+		 * Show only sticky posts
+		 * as this will display on all pages.
+		 */
+		$sticky_query = new WP_Query( array( 'post__in' => get_option( 'sticky_posts' ) ) );
+
+		if ( $sticky_query->have_posts() ) {
 
 			/* Start the loop */
-			while ( have_posts() ) {
+			while ( $sticky_query->have_posts() ) {
 
-				the_post();
+				$sticky_query->the_post();
+				get_template_part( 'template-parts/post/content', get_post_format() );
+
+			}
+		}
+		?>
+
+		</div>
+
+		<div class="site-blog site-blog-bar">
+
+		<?php
+		if ( $the_query->have_posts() ) {
+
+
+			/* Start the loop */
+			while ( $the_query->have_posts() ) {
+
+				$the_query->the_post();
 
 				/**
 				 * Use the Post-Formats support module to
--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -15,13 +15,29 @@
 		<header class="entry-header">
 
 			<?php
+			/**
+			 * Do not show dot next to title if sticky,
+			 * since sticky posts look different
+			 * compared to normal posts.
+			 */
+			$dot = is_sticky() ? '' : 'site-blog-dot';
+
+			/**
+			 * Singular post titles look different to
+			 * blog content.
+			 */
 			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
+				$title_start = '<h1 class="entry-title">';
+				$title_end   = '</h1>';
 			} elseif ( is_front_page() || is_home() ) {
-				the_title( '<h3 class="entry-title site-blog-dot"><a class="entry-title-link" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+				$title_start = '<h3 class="entry-title ' . $dot . '"><a class="entry-title-link" href="' . esc_url( get_permalink() ) . '" rel="bookmark">';
+				$title_end   = '</a></h3>';
 			} else {
-				the_title( '<h3 class="entry-title"><a class="entry-title-link" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+				$title_start = '<h3 class="entry-title"><a class="entry-title-link" href="' . esc_url( get_permalink() ) . '" rel="bookmark">';
+				$title_end   = '</a></h3>';
 			}
+
+			the_title( $title_start, $title_end );
 			?>
 
 		</header>
@@ -37,15 +53,27 @@
 
 		</div>
 		<?php
+		/**
+		 * Check if post is a post
+		 * and display post meta.
+		 */
 		if ( 'post' === get_post_type() ) {
 			echo '<div class="entry-meta">';
+
+			// Display avatar.
 			echo '<img class="entry-author-avatar" src="' . esc_url( get_avatar_url( get_the_author_meta( 'email' ) ) ) . '" alt="profile_pic">';
+
+			// Display nice name.
+			echo esc_html( get_the_author_meta( 'user_nicename' ) ) . ' ';
+
+			// Only show edit button when post is visited.
 			if ( is_single() ) {
 				jhuy_posted_on();
 			} else {
 				echo jhuy_time_link();
 				jhuy_edit_link();
 			};
+
 			echo '</div><!-- .entry-meta -->';
 		};
 		?>
