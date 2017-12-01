1204e0dd31360e6a6fb12f5f64831bb2d7d14da2
Johnny Huynh <johnnyhuy@users.noreply.github.com>
2017-12-01 21:11:47 +1100

    Fixed time string gap.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -22,7 +22,7 @@ if ( ! function_exists( 'jhuy_time_link' ) ) :
 	function jhuy_time_link( $elapsed = false ) {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%4$s ago</time>';
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>, <time class="updated" datetime="%3$s">%4$s ago</time>';
 		}
 
 		$time_string = sprintf( $time_string,
